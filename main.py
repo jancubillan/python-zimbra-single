fd21bb1eb0470ce74a24ee781137742e312b8bff
Jan Cubillan <jan.cubillan@gmail.com>
2020-05-16 19:00:58 +0800

    fixed typos

--- main.py
+++ main.py
@@ -281,4 +281,4 @@ zimbra_require_interprocess_security="1"
 INSTALL_PACKAGES="zimbra-core zimbra-ldap zimbra-logger zimbra-mta zimbra-snmp zimbra-store zimbra-apache zimbra-spell zimbra-memcached zimbra-proxy "
 """
 
-sp.run(["/opt/zimbra/libexec/zmsetup.pl". "-c", "../zimbra_config.txt"])
+sp.run(["/opt/zimbra/libexec/zmsetup.pl", "-c", "../zimbra_config.txt"])
